5ea32fd4e62a82b2ace7e2c18062e269d4879f49
chareesa <chareesagraham@gmail.com>
2017-12-12 02:42:46 -0800

    fix dependency errors

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log('server running on port ' + 8080);
